0cacba3dd348da260e02c990dae396dc89e865ca
M1ster Rob0t <baks750@mail.ru>
2024-01-14 10:45:33 +0300

    Настроил маршруты для приложения на React Router Dom v.5

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+import './style.css';
+
+import ErrorImage from './image/sad-cat.png';
+import { AppRoute } from '../../utils/constants';
+
+function NotFound(): JSX.Element {
+  return (
+    <div className="not-found">
+      <h1 className="not-found__title">
+        <img src={ErrorImage} width="50" height="50" style={{ marginRight: '10px' }} alt="cat" />
+        404. Page not found...
+      </h1>
+      <p className="not-found__message-additional">
+        Go to <Link to={AppRoute}>Main Page</Link>
+      </p>
+    </div>
+  );
+}
+
+export default NotFound;
